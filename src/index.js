d8bfccbeff768f177580fcbe77a59c5a92d47613
notregret <sanaxxx322@gmail.com>
2021-02-20 01:13:08 +0300

    fix: fix mistakes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+    let min = 1000000;
+    if ((array === undefined) || (array.length === 0)){
+        return 0
+    }else{
+    for(let i = 0; i < array.length; i++){
+        if (array[i] < min){
+            min = array[i]
+        }
+    }}
+
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    let max = 0;
+    if ((array === undefined) || (array.length === 0)){
+        return 0
+    }else{
+        for(let i = 0; i < array.length; i++){
+            if (array[i] > max){
+                max = array[i]
+            }
+        }}
+
+    return max;
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+    let average = 0;
+    if ((array === undefined) || (array.length === 0)){
+        return 0
+    }else{
+        for(let i = 0; i < array.length; i++){
+            average += array[i];
+        }
+    }
+
+    return average / array.length;
 }
+
+
